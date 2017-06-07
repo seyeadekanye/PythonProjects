1dfbf692ec33523d01e32cc5baae3ef4837f4c75
seyeadekanye <seyeadekanye@gmail.com>
2017-06-07 09:37:51 -0400

    Delete change_money.py

--- change_money.py
+++
@@ -1,8 +0,0 @@
-# Ask user if they want to buy or sell dollars
-# Then ask for amount
-# then call appropriate function and do conversion
-# use t_date = date.today() to display date
-
-import fx
-
-from datetime import date
